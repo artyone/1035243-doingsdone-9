1e0636e61595962b4ae4c12f47d647645c3a41f4
gizzmaster <gizzmasater@gmail.com>
2019-04-11 20:19:28 +0300

    Исправление неправильного использование шорт тега

--- index.php
+++ index.php
@@ -93,18 +93,18 @@ $show_complete_tasks = rand(0, 1);
                         <td class="task__date"></td>
                     </tr>
                     <!--Показывает дополнительный тег <tr/> выполненного задания, если переменная $show_complete_tasks равна единице-->
-                    <? if ($show_complete_tasks) : ?>
-                    	<tr class="tasks__item task task--completed">
-                        	<td class="task__select">
-                            	<label class="checkbox task__checkbox">
-	                                <input class="checkbox__input visually-hidden" type="checkbox" checked>
-	                                <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
-                            	</label>
-                        	</td>
-	                        <td class="task__date">10.10.2019</td>
-	                        <td class="task__controls"></td>
-                   		</tr>
-                    <? endif; ?>
+                    <?php if ($show_complete_tasks) : ?>
+                    <tr class="tasks__item task task--completed">
+                        <td class="task__select">
+                            <label class="checkbox task__checkbox">
+	                        	<input class="checkbox__input visually-hidden" type="checkbox" checked>
+	                        	<span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                            </label>
+                        </td>
+	                    <td class="task__date">10.10.2019</td>
+	                    <td class="task__controls"></td>
+                   	</tr>
+                    <?php endif; ?>
                 </table>
             </main>
         </div>
