44d32dcb16cc718b95951cb188542dab3857e38b
gizzmaster <gizzmasater@gmail.com>
2019-04-21 12:24:10 +0300

    исправления по созданию БД

--- schema.sql
+++ schema.sql
@@ -1,34 +1,42 @@
 CREATE DATABASE doingdone
-CHARACTER SET UTF8
-COLLATE UTF8_GENERAL_CI;
+    CHARACTER SET UTF8
+    COLLATE UTF8_GENERAL_CI;
+
 USE doingdone;
-CREATE TABLE task (
-	id TINYINT NOT NULL AUTO_INCREMENT,
-	create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-	status TINYINT(1),
-	name VARCHAR(500) NOT NULL,
-	file_link TEXT DEFAULT NULL,
-	expiration_time DATETIME DEFAULT NULL,
-	project_id TINYINT NOT NULL,
-	user_id TINYINT NOT NULL,
-	PRIMARY KEY (id),
-	INDEX (name)
+
+CREATE TABLE user
+(
+    id          INT          NOT NULL AUTO_INCREMENT,
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    email       VARCHAR(254) NOT NULL,
+    name        VARCHAR(500) NOT NULL,
+    password    VARCHAR(500) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (email)
 );
 
-CREATE TABLE project (
-	id TINYINT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(500) NOT NULL,
-	user_id TINYINT NOT NULL,
-	PRIMARY KEY (id),
-	UNIQUE (name)
+CREATE TABLE project
+(
+    id      INT          NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(500) NOT NULL,
+    user_id INT          NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (name)
 );
 
-CREATE TABLE user (
-	id TINYINT(4) NOT NULL AUTO_INCREMENT,
-	create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-	email VARCHAR(254) NOT NULL,
-	name VARCHAR(500) NOT NULL,
-	password VARCHAR(500) NOT NULL,
-	PRIMARY KEY (id),
-	UNIQUE (email)
+CREATE TABLE task
+(
+    id              INT          NOT NULL AUTO_INCREMENT,
+    create_time     DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status          TINYINT,
+    name            VARCHAR(500) NOT NULL,
+    file_link       TEXT     DEFAULT NULL,
+    expiration_time DATETIME DEFAULT NULL,
+    project_id      INT          NOT NULL,
+    user_id         INT          NOT NULL,
+    PRIMARY KEY (id),
+    INDEX (name)
 );
+
+
+
