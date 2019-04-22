2429ba669fcf90bf6f88d98c19773078a74bd657
gizzmaster <gizzmasater@gmail.com>
2019-04-22 23:39:28 +0300

    последние изменения

--- schema.sql
+++ schema.sql
@@ -23,19 +23,32 @@ CREATE TABLE project
     PRIMARY KEY (id)
 );
 
+ALTER TABLE project ADD UNIQUE project_name_user_id_udx(name, user_id);
+
 CREATE TABLE task
 (
     id              INT          NOT NULL AUTO_INCREMENT,
     create_time     DATETIME DEFAULT CURRENT_TIMESTAMP,
-    status          TINYINT, DEFAULT 0
+    status          TINYINT DEFAULT 0,
     name            VARCHAR(500) NOT NULL,
     file_link       VARCHAR(500) DEFAULT NULL,
     expiration_time DATETIME DEFAULT NULL,
-    project_id      INT          NOT NULL,
     user_id         INT          NOT NULL,
+    project_id      INT          NOT NULL,
     PRIMARY KEY (id),
-    INDEX (name)
+    INDEX (name),
+    INDEX (status)
 );
 
+ALTER TABLE project
+    ADD FOREIGN KEY (user_id)
+        REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
+ALTER TABLE task
+    ADD FOREIGN KEY (user_id)
+        REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE task
+    ADD FOREIGN KEY (project_id)
+        REFERENCES project(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
