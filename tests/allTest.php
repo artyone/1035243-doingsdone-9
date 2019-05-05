789b83d51e4f4fc823db0c68632709fab2af3c78
gizzmaster <gizzmasater@gmail.com>
2019-05-05 15:56:06 +0300

    исправления по комментариям 2

--- tests/allTest.php
+++ tests/allTest.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once '../tests/isImportantTest.php';
-require_once '../tests/getUserTest.php';
+require_once '../tests/getUserByIdTest.php';
 require_once '../tests/getProjectsTest.php';
 require_once '../tests/getTasksTest.php';
 require_once '../tests/getProjectTest.php';
